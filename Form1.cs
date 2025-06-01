afe2d0a14094b118f4b1d387b99f207a12168990
Aphisit Namracha <aphisit.namracha@outlook.com>
2025-06-01 23:17:30 +0700

    initial commit

---
+++ Form1.cs
@@ -0,0 +1,100 @@
+using System.Runtime.InteropServices;
+
+namespace CursorLockerTrayApp;
+
+public partial class Form1 : Form
+{
+    private NotifyIcon trayIcon;
+    private ContextMenuStrip trayMenu;
+    private Thread lockThread;
+    private bool isLocking = false;
+    private bool stopRequested = false;
+
+    [DllImport("user32.dll")]
+    static extern bool SetCursorPos(int X, int Y);
+
+    [DllImport("user32.dll")]
+    static extern short GetAsyncKeyState(Keys vKey);
+
+    public Form1()
+    {
+        InitializeComponent();
+
+        trayMenu = new ContextMenuStrip();
+        trayMenu.Items.Add("Start Lock", null, ToggleLock);
+        trayMenu.Items.Add("Exit", null, ExitApp);
+
+        trayIcon = new NotifyIcon()
+        {
+            Text = "Cursor Locker",
+            Icon = new Icon("trayicon.ico"),
+            ContextMenuStrip = trayMenu,
+            Visible = true
+        };
+
+        // Hide main form
+        this.WindowState = FormWindowState.Minimized;
+        this.ShowInTaskbar = false;
+        this.FormBorderStyle = FormBorderStyle.FixedToolWindow;
+        this.Load += (s, e) => this.Hide();
+    }
+
+    private void ToggleLock(object? sender, EventArgs e)
+    {
+        if (!isLocking)
+        {
+            StartLocking();
+            trayMenu.Items[0].Text = "Stop Lock";
+        }
+        else
+        {
+            StopLocking();
+            trayMenu.Items[0].Text = "Start Lock";
+        }
+    }
+
+    private void StartLocking()
+    {
+        stopRequested = false;
+        isLocking = true;
+        lockThread = new Thread(() =>
+        {
+           while (!stopRequested)
+        {
+            // Move cursor to center
+            int centerX = Screen.PrimaryScreen.Bounds.Width / 2;
+            int centerY = Screen.PrimaryScreen.Bounds.Height / 2;
+            SetCursorPos(centerX, centerY);
+
+            // Check if ESC is pressed
+            if ((GetAsyncKeyState(Keys.Escape) & 0x8000) != 0)
+            {
+                Invoke(() =>
+                {
+                    StopLocking();
+                    trayMenu.Items[0].Text = "Start Lock";
+                    trayIcon.ShowBalloonTip(1000, "Cursor Locker", "Lock stopped (ESC pressed)", ToolTipIcon.Info);
+                });
+                break;
+            }
+
+            Thread.Sleep(10);
+        }
+        });
+        lockThread.IsBackground = true;
+        lockThread.Start();
+    }
+
+    private void StopLocking()
+    {
+        stopRequested = true;
+        isLocking = false;
+    }
+
+    private void ExitApp(object? sender, EventArgs e)
+    {
+        StopLocking();
+        trayIcon.Visible = false;
+        Application.Exit();
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,21 @@
+# CursorLockerTrayApp
+
+Prerequired:
+- .NET 8
+
+Start on local:
+```sh
+dotnet run
+```
+
+Publish to EXE:
+```sh
+dotnet publish -c Release -r win-x64 --self-contained true
+```
+
+Publish into single file:
+```sh
+dotnet publish -c Release -r win-x64 --self-contained true /p:PublishSingleFile=true /p:Trim=true
+```
+
+If you need to stop lock the cursor, press `ESC`.
